87d4576da431c5400423a7ae2ec82974bee19b8e
Auba Fuster <aubafp@gmail.com>
2024-02-21 00:34:26 +0100

    get in touch button to redirect to contact page

---
+++ index.js
@@ -0,0 +1,11 @@
+// Get button
+
+const getInTouchBtn = document.querySelector("#getBtn")
+
+// Add event into Get in Touch button to navigate
+
+getInTouchBtn.addEventListener('click',() => {
+    navigateToPage(contactPage) 
+    /* navigateToPage function defined in general.js file */
+    /* variable contactPage defined in general.js file */
+})
