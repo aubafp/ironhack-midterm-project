d68d0811b6efa92b62ec74f08f688c40354b19cf
Auba Fuster <aubafp@gmail.com>
2024-02-21 00:59:43 +0100

    functions validateName and hasNumbers developed, pending tests

--- Contact/contact.js
+++ Contact/contact.js
@@ -11,14 +11,29 @@ const messageInput = document.querySelector("#message")
 
 // Functions for form validation
 
+// Function to check if an array contains any number
+function hasNumbers(input) {
+    return /\d/.test(input) 
+    // \d is a metacharacter that matches any digit from 0 to 9 (RegEx)
+    // The test() method of RegExp instances executes a search with this regular expression for a match between a regular expression and a specified string. Returns true if there is a match; false otherwise.
+    // should return true if the array contains any number
+    // should return false if the array contains no numbers
+}
+
+// Function to validate the name
 function validateName(input) {
     if (!input) {
         return "The name is compulsory"
     }
-    // compulsory to have a value
-    // not ironhack
-    // no numbers?
-
+    else if (input.toLowerCase() === "ironhack") {
+        return "You cannot be Ironhack, because I am Ironhack"
+    }
+    else if (hasNumbers(input) === true) {
+        return "A name cannot contain numbers"
+    }
+    else {
+        return true
+    }
     // should return true if all is OK
     // should return the alert message if something is not OK
 }
@@ -60,13 +75,17 @@ function validateForm() {
     // should return the alert message if something is not OK
 }
 
+function showAlert(message) {
+    // show alert receiving the alert message by parameter
+}
+
 function sendInformation() {
     if (validateForm() == true) {
         // send information
     }
 
     else {
-        // show alert taking the message from validateForm() return
+        showAlert(validateForm())
     }
 }
 
