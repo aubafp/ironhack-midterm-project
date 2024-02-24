818f9ec3344b353ed226b3791d382ca12004f964
Auba Fuster <aubafp@gmail.com>
2024-02-24 12:31:32 +0100

    project.js de URL id and  treat API answer data.Ppending DOM modification of project detail and other projects section

--- Project/project.js
+++ Project/project.js
@@ -5,23 +5,54 @@ const apiUrl = "https://raw.githubusercontent.com/ironhack-jc/mid-term-api/main/
 async function getServerProjectsInfo(url) {
     try {
         const response = await fetch(url)
-        return response.json()
+        return await response.json()
     } catch (err) {
-        console.log(err.message);
+        alert(err.message);
     }
+    // on success it returns the API response parsed into json format
 }
 
+// Function to get Id from URL using URLSearchParams functionality
 function getIdFromUrl() {
-    
+    const url = window.location.search // get current URL
+    const searchParams = new URLSearchParams(url); // create instance of URLSearchParams class
+    return searchParams.get("id") // use the class method get to take the url parameter called id and return the id value
+    // it returns the value of the url parameter called id
 }
+
+// Function to get object with current page information (id of page) - return object with current project information
+function getCurrentProjectData(allProjects, id) {
+    const currentProject = allProjects.filter((project) => {
+        return project.uuid === id
+    })
+    return currentProject[0]
+    // returns an object with currrent project data
+}
+
+// Function to get array with ALL projects except of current project 
+function getOtherProjectsData(allProjects, id) {
+    const otherProjects = allProjects.filter((project) => {
+        return project.uuid != id
+    })
+    return otherProjects
+    // returns an array of objects with the others projects data
+}
+
 
 // Function to call in the event listener that will call to all necessary operations to display the correct dynamic project detail page content
 async function loadPageContent() {
-    const projectsInfo = await getServerProjectsInfo(apiUrl)
-    // function to get Id from URL -> usar https://developer.mozilla.org/en-US/docs/Web/API/URLSearchParams 
-    // function to convert the server response into nice data filtering by current project (Id of URL) - return an object with the current project data
+    const allProjectsInfo = await getServerProjectsInfo(apiUrl)
+    const id = getIdFromUrl()
+    const currentProject = getCurrentProjectData(allProjectsInfo, id)
+    const otherProjects = getOtherProjectsData(allProjectsInfo, id)
+
+    console.log(currentProject)
+    console.log(otherProjects)
+
+
     // function for DOM modification of HTML to add the project content
-    // function to convert the server response into nice data filtering by ALL projects except of current project (Id of URL) - return an array of objects with the others projects data
+
+
     // function for DOM modification of HTML to add the Other projects content (should never be the same of the project where I am, so take the other 3 available projects)
 }
 
